0d1919e710867371c64ecceb3fe79d575b55a5c7
Musangamfura Emmanuel <musangamfure@gmail.com>
2024-11-10 21:56:04 +0200

    Setup and project development

---
+++ app/(home)/about/page.tsx
@@ -0,0 +1,58 @@
+export default function AboutPage() {
+  return (
+    <div className="px-6 lg:w-5/6 xl:w-4/6 py-10 mx-auto">
+      <div className="space-y-12 text-md md:text-lg text-gray-500 pb-4 md:pb-8">
+        <div className="space-y-2">
+          <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+            💪 Workout Program
+          </h2>
+          <p>
+            Our workout programs are designed to help you achieve your fitness
+            goals quickly and easily. Whether you want to build muscle, increase
+            endurance, or improve your overall fitness, our diverse workouts
+            offer a personalized exercise regimen that fits your needs and
+            schedule.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+            🥗 Diet Program
+          </h2>
+          <p>
+            Achieve optimal results with our personalized diet programs that
+            complement your workout. Our nutrition experts have created dietary
+            plans for various fitness preferences and goals, helping you stay on
+            track for your health and wellness journey.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+            📊 Weight Analysis
+          </h2>
+          <p>
+            Stay informed about your progress with our advanced weight
+            analytics. Track weight changes, set achievable goals, and monitor
+            your weight loss or muscle gain journey. Our detailed analytics
+            provide valuable insights to make informed decisions and adjustments
+            to your fitness program.
+          </p>
+        </div>
+
+        <div className="space-y-2">
+          <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+            🏋️‍♀️ Body Analysis
+          </h2>
+          <p>
+            Understand your body better with our detailed body composition
+            analytics. Track changes in muscle mass, body fat percentage, and
+            overall body composition to understand your physical progress. Our
+            analytics help you make informed choices and adjustments to improve
+            your fitness routine.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
